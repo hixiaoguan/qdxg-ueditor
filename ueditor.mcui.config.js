4dc0965d67deb1c860fd068b17bf3214d83c579d
官士彬 <guanshibin@qing5.comproductdeMacBook-Pro:qdzkcm_website_template>
2016-03-17 19:57:20 +0800

    +qdxg-ueditor-php-1.4.3.2
    百度编辑器ueditor皮肤修改

---
+++ ueditor.mcui.config.js
@@ -0,0 +1,31 @@
+var ue = UE.getEditor('editor', {
+toolbars: [
+    [      
+        'fullscreen',
+        'source',
+        'undo',
+        'redo',
+        'bold',
+        'italic', 
+        'underline', 
+        'blockquote',
+        'removeformat',
+        'fontsize',
+        'forecolor',
+        'backcolor',
+        'simpleupload',
+        'insertimage',
+        'link',
+        'unlink',
+        'justifyleft',
+        'justifycenter',
+        'justifyright',
+        'indent',
+        'inserttable',
+        'insertorderedlist',
+        'insertunorderedlist'
+    ]
+],
+autoHeightEnabled: true,
+autoFloatEnabled: true
+});
